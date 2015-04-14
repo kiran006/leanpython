e26475970adf29e33cced7989c1021b287a4222d
kiran006 <bhulokam.kirankumar@gmail.com>
2015-04-14 13:09:04 +0000

    assign-2(14-04-2015)

---
+++ ch1/strings.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+
+##1st
+
+a="kiran"
+b="kriankumar"
+len(a)-1
+b[5:]
+
+##2nd
+
+a="hello \n"
+print a
+
+
+##3rd q's
+a =" Hello World"
+print a.replace("Hello", "Good Bye")
+
+
+
