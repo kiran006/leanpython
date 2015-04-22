f3013872042cccfa6a51c117466779e48f4cc12f
kiran006 <bhulokam.kirankumar@gmail.com>
2015-04-22 04:53:07 +0000

    new file added

--- ch5_files/task.py
+++ ch5_files/task.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python
 
-#open a input file.
-open("/etc/passwd")
-# stored open file in pfile variable.
-pfile = open("/etc/passwd")
-#print pfile data.
-print pfile
-#asssign pfile readlines to plist.
-plist = pfile.readlines()
-#print plist.
-print plist
-#create for loop.
-for i in range(0,len(plist)):
-    print i+1,plist[i].strip()
-    
+
+open("file.txt","r+")
+f=open("file.txt","r+")
+f.readlines()
+list=f.readlines()
+
+for i in range(0,(len(list))):
+    print i
+
